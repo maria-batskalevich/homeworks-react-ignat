737f12fa09e24079e6c4f043963707e26b8a1963
maria-batskalevich <maria.batskalevich@gmail.com>
2021-09-25 21:58:17 +0300

    update package.json, started doing homework 5

---
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import HW1 from '../../h1/HW1'
+import HW2 from '../../h2/HW2'
+import HW3 from '../../h3/HW3'
+import HW4 from '../../h4/HW4'
+
+function Junior() {
+    return (
+        <div>
+            {/*<HW7/>*/}
+            {/*<HW8/>*/}
+            {/*<HW9/>*/}
+            {/*<HW10/>*/}
+            {/*<HW11/>*/}
+
+
+        </div>
+    )
+}
+
+export default Junior
+
+// сделайте по аналогии пустые страницы джун и джун+
+// туда будут добавляться следующие дз
