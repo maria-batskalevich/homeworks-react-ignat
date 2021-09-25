b7ed14410d4bd51b66e4440e9362650d38433f63
maria-batskalevich <maria.batskalevich@gmail.com>
2021-09-25 22:07:05 +0300

    add error404 page

--- src/p2-homeworks/h5/pages/Error404.tsx
+++ src/p2-homeworks/h5/pages/Error404.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import './error.module.css'
 
 function Error404() {
     return (
         <div>
-            <div>404</div>
-            <div>Page not found!</div>
-            <div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>
+            <h1>404 Error Page #1</h1>
+            <p className="zoom-area"><b>CSS</b> animations to make a cool 404 page. </p>
+            <section className="error-container">
+                <span><span>4</span></span>
+                <span>0</span>
+                <span><span>4</span></span>
+            </section>
+            <div className="link-container">
+                <a target="_blank" href="https://www.silocreativo.com/en/creative-examples-404-error-css/"
+                   className="more-link">Visit the original article</a>
+            </div>
         </div>
     )
 }
