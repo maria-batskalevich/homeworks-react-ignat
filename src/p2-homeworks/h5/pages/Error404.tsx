783ce525c6ad7beeab5c997189a0d2c0768f260a
maria-batskalevich <maria.batskalevich@gmail.com>
2021-09-25 22:22:44 +0300

    add error404 page

--- src/p2-homeworks/h5/pages/Error404.tsx
+++ src/p2-homeworks/h5/pages/Error404.tsx
@@ -4,8 +4,8 @@ import './error.module.css'
 function Error404() {
     return (
         <div>
-            <a href="" className="fa fa-arrow-left"></a>
-            <div className="error">
+            <a href="" className={"fa fa-arrow-left"}></a>
+            <div className={"error"}>
                 <h1>404</h1>
                 <p>We're sorry but it looks like that page doesn't exist anymore.</p>
             </div>
