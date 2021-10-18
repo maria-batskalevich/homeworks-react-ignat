6d38f73aeec70c835d341cf01633d46df0cf3765
maria-batskalevich <maria.batskalevich@gmail.com>
2021-10-18 18:39:16 +0300

    add homework7

--- src/p2-homeworks/h5/pages/Junior.tsx
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
+import HW7 from "../../h7/HW7";
 
 function Junior() {
     return (
         <div>
-
-            {/*<HW7/>*/}
+            <HW7/>
             {/*<HW8/>*/}
             {/*<HW9/>*/}
             {/*<HW10/>*/}
@@ -15,5 +15,3 @@ function Junior() {
 
 export default Junior
 
-// сделайте по аналогии пустые страницы джун и джун+
-// туда будут добавляться следующие дз
