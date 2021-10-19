5d6f2957de89c154cfb3f52e1b8bc7724d8c8e9a
maria-batskalevich <maria.batskalevich@gmail.com>
2021-10-19 15:46:01 +0300

    add homework8

--- src/p2-homeworks/h8/HW8.tsx
+++ src/p2-homeworks/h8/HW8.tsx
@@ -1,29 +1,38 @@
 import React, {useState} from 'react'
 import {homeWorkReducer} from './bll/homeWorkReducer'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import s from './HW8.module.css'
 
-// export type UserType =
 
-const initialPeople = [
-    {_id: 0, name: 'Кот', age: 3},
-    {_id: 1, name: 'Александр', age: 66},
-    {_id: 2, name: 'Коля', age: 16},
-    {_id: 3, name: 'Виктор', age: 44},
-    {_id: 4, name: 'Дмитрий', age: 40},
-    {_id: 5, name: 'Ирина', age: 55},
+export type UserType = {
+    _id: number
+    name: string
+    age: number
+}
+
+const initialPeople: UserType[] = [
+    {_id: 0, name: 'Maria', age: 31},
+    {_id: 1, name: 'Emma', age: 22},
+    {_id: 2, name: 'Sophia', age: 16},
+    {_id: 3, name: 'Isabella', age: 64},
+    {_id: 4, name: 'Mia', age: 35},
+    {_id: 5, name: 'Olivia', age: 45},
 ]
 
 function HW8() {
-    const [people, setPeople] = useState<any>(initialPeople) // need to fix any
+    const [people, setPeople] = useState<UserType[]>(initialPeople)
 
     // need to fix any
-    const finalPeople = people.map((p: any) => (
-        <div key={p._id}>
-            some name, age
+    const finalPeople = people.map((p: UserType) => (
+        <div key={p._id} className={s.item}>
+            <span>{p.name}</span>
+            {p.age}
         </div>
     ))
 
     const sortUp = () => setPeople(homeWorkReducer(initialPeople, {type: 'sort', payload: 'up'}))
+    const sortDown = () => setPeople(homeWorkReducer(initialPeople, {type: 'sort', payload: 'down'}))
+    const check18 = () => setPeople(homeWorkReducer(initialPeople, {type: 'check', payload: 18}))
 
     return (
         <div>
@@ -33,9 +42,13 @@ function HW8() {
             {/*should work (должно работать)*/}
             {finalPeople}
 
-            <div><SuperButton onClick={sortUp}>sort up</SuperButton></div>
-            <div>sort down</div>
-            check 18
+            <div>
+                <SuperButton onClick={sortUp}>sort up</SuperButton>
+                <SuperButton onClick={sortDown}>sort down</SuperButton>
+                <SuperButton onClick={check18}>check 18</SuperButton>
+            </div>
+            {/*<div>sort down</div>*/}
+            {/*check 18*/}
 
             <hr/>
             {/*для личного творчества, могу проверить*/}
--- src/p2-homeworks/h8/bll/tests/homeWorkReducer.test.tsx
+++ src/p2-homeworks/h8/bll/tests/homeWorkReducer.test.tsx
@@ -5,28 +5,30 @@ let initialState: any[] // need to fix any
 
 beforeEach(() => {
     initialState = [
-        {_id: 0, name: 'Кот', age: 3},
-        {_id: 1, name: 'Александр', age: 66},
-        {_id: 2, name: 'Коля', age: 16},
-        {_id: 3, name: 'Виктор', age: 44},
-        {_id: 4, name: 'Дмитрий', age: 40},
-        {_id: 5, name: 'Ирина', age: 55},
+        {_id: 0, name: 'Maria', age: 31},
+        {_id: 1, name: 'Emma', age: 22},
+        {_id: 2, name: 'Sophia', age: 16},
+        {_id: 3, name: 'Isabella', age: 64},
+        {_id: 4, name: 'Mia', age: 35},
+        {_id: 5, name: 'Olivia', age: 45},
     ]
 })
 
 test('sort name up', () => {
     const newState = homeWorkReducer(initialState, {type: 'sort', payload: 'up'})
+    const result = [
+        1, 3, 4, 5, 2, 0
+    ].filter((el, i) => el === newState[i]._id)
+    expect(result.length).toBe(newState.length)
+    expect(newState[0]._id).toBe(1)
 
-    console.log(newState)
-    // expect(...).toBe(...)
 })
 test('sort name down', () => {
     const newState = homeWorkReducer(initialState, {type: 'sort', payload: 'down'})
-
+    expect(newState[0]._id).toBe(0)
 
 })
 test('check age 18', () => {
     const newState = homeWorkReducer(initialState, {type: 'check', payload: 18})
-
-
+    expect(newState.length).toBe(4)
 })
